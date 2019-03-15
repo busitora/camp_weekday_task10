7112e686c3eb1d0e784024798701141ba76414bb
busitora <ts12272530@gmail.com>
2019-03-15 15:07:26 +0900

    課題10作成

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -1,11 +1,42 @@
 class AreasController < ApplicationController
-
-  def index
-  end
+  # apiリクエストが送れるようになるやつ
+  require 'net/http'
+  # URI::HTTPの形に整形してくれるやつ
+  require 'uri'
+  # api_requestメソッドを使用可能にする
+  include AreasHelper
   
+  def index
+    @areas = Area.all
+  end
+
   def search
   end
 
-  def create
+  def form
+    zipcode = params[:zipcode]
+    # getリクエストで遷移してきたときは処理させない条件分岐
+    if zipcode
+      @area = Area.new
+      # areas_helper.rbにメソッドを記述
+      api_request(zipcode)
+    end
   end
-end
+
+  def create
+    @area = Area.new(area_params)
+    # @areaのバリデーション判定
+    if @area.save
+      flash[:notice] = '地域を登録しました。'
+      redirect_to root_path
+    else
+      flash.now[:alert] = "Validation failed: #{@area.errors.full_messages.join}"
+      render :form
+    end
+  end
+
+  private
+    def area_params
+      params.require(:area).permit!
+    end
+  end
